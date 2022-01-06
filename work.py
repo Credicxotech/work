360fb0deefe94d3fc43a78e0babf2995605cb67a
Credicxotech <93118326+Credicxotech@users.noreply.github.com>
2022-01-06 15:56:35 +0530

    Update work.py

--- work.py
+++ work.py
@@ -5,7 +5,9 @@ PROXY = "127.0.0.1:22999" # IP:PORT or HOST:PORT
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--proxy-server=%s' % PROXY)
 
-chrome = webdriver.Chrome(options=chrome_options)
+chrome = webdriver.Chrome(options=chrome_options, executable_path=r'/root/facebook_vist/chromedriver')
 chrome.get("http://whatismyipaddress.com")
+
+
 
 
